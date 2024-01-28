43d83d6f79294098d5a1628fd706e84eaa15b9c9
Your Name <taravkaa@gmail.com>
2024-01-28 21:50:55 +0300

    Removed collections usage

--- Program.cs
+++ Program.cs
@@ -1,27 +1,36 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-
-
-class Program
+﻿class Program
 {
     private static string[] input = new string[] {"Moscow", "123", "abcd", "-1", "234", "Berlin"};
 
     public static void Main()
     {
 
-        var output = new List<string>();
+        var output = new string[input.Length];
+        int count = 0;
 
         for (var i = 0; i < input.Length; i++)
         {
             if (input[i].Length <= 3)
             {
-                output.Add(input[i]);
+                output[i] = input[i];
+                count = count + 1;
+            }
+
+        }
+
+        var final = new string[count];
+        var index = 0;
+
+        foreach (var s in output)
+        {
+            if (s != null)
+            {
+                final[index] = s;
+                index = index + 1;
             }
         }
 
-        var result = output.ToArray();
-
         Console.WriteLine("Results: ");
-        Console.WriteLine(string.Join(' ', result));
+        Console.WriteLine(string.Join(' ', final));
     }
 }
