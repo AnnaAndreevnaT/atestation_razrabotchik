4b6350f512ce664602073e94792eccaacf5cc8ff
Your Name <taravkaa@gmail.com>
2024-01-28 21:33:14 +0300

    Added implementation

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections.Generic;
+using System.Linq;
+
+
+class Program
+{
+    private static string[] input = new string[] {"Moscow", "123", "abcd", "-1", "234", "Berlin"};
+
+    public static void Main()
+    {
+
+        var output = new List<string>();
+
+        for (var i = 0; i < input.Length; i++)
+        {
+            if (input[i].Length <= 3)
+            {
+                output.Add(input[i]);
+            }
+        }
+
+        var result = output.ToArray();
+
+        Console.WriteLine("Results: ");
+        Console.WriteLine(string.Join(' ', result));
+    }
+}
